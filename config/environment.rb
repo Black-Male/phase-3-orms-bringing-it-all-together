9cc8445856af14e752ef36cf27ad8e46ae5b32ed
Black-Male <Gkaranja20@gmail.com>
2023-02-23 17:28:01 +0300

    Done

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,8 @@ Bundler.require
 require_relative '../lib/dog'
 
 DB = { conn: SQLite3::Database.new("db/dogs.db") }
+
+
+# Dog.create_table
+# jim = Dog.new(name: "Jim", breed: "retard")
+# jim.save
